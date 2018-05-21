e4c2579bcc3298b09a5878836622fedb18cbcef3
Irina112583 <irik9728@gmail.com>
2018-05-21 15:26:52 +0300

    Update server-core.js

--- server-core.js
+++ server-core.js
@@ -3,10 +3,51 @@
 const http = require('http');
 
 const server = http.createServer();
+const url = require('url');
+const queryString = require('querystring');
+const commands = {
+    GET: get,
+    POST: post
+};
+
+let messages = [];
 
 server.on('request', (req, res) => {
-    // Тут нужно обработать запрос
-    res.end();
+    if ((/^\/messages(\/|\?|$)/).test(req.url)) {
+        res.setHeader('Content-Type', 'application/json');
+        commands[req.method](req, res);
+    } else {
+        res.statusCode = 404;
+        res.end();
+    }
 });
+
+function get(req, res) {
+    let query = url.parse(req.url).query;
+    let { from, to } = queryString.parse(query);
+
+    let posts = messages.filter(message =>
+        (!from || from === message.from) &&
+        (!to || to === message.to));
+    res.end(JSON.stringify(posts));
+}
+
+function post(req, res) {
+    let query = url.parse(req.url).query;
+    let { from, to } = queryString.parse(query);
+    let message = {};
+    let text = '';
+    req
+        .on('data', data => {
+            text += data;
+        })
+        .on('end', () => {
+            message.from = from;
+            message.to = to;
+            message.text = JSON.parse(text).text;
+            messages.push(message);
+            res.end(JSON.stringify(message));
+        });
+}
 
 module.exports = server;
